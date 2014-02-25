5a542ac73f685c8b1390ba06c0dace9065a5b479
Graylin Kim <graylin.kim@gmail.com>
2014-02-25 14:46:38 -0500

    Generate summary tables for faster reporting.

--- resources/schema.sql
+++ resources/schema.sql
@@ -1,5 +1,12 @@
 DROP TABLE IF EXISTS request;
 DROP TABLE IF EXISTS summary_1m;
+DROP TABLE IF EXISTS uniques_1m;
+DROP TABLE IF EXISTS summary_15m;
+DROP TABLE IF EXISTS uniques_15m;
+DROP TABLE IF EXISTS summary_1h;
+DROP TABLE IF EXISTS uniques_1h;
+DROP TABLE IF EXISTS summary_1d;
+DROP TABLE IF EXISTS uniques_1d;
 DROP TABLE IF EXISTS instance;
 DROP TABLE IF EXISTS apache_cron_runs;
 
@@ -29,6 +36,114 @@ CREATE TABLE request (
   INDEX(time),
   INDEX(remote_ip),
   FOREIGN KEY (instance_id) REFERENCES instance(id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+CREATE TABLE summary_1m (
+    time datetime,
+    remote_ip varchar(255),
+    instance_id int unsigned,
+    503_errors int unsigned,
+    500_errors int unsigned,
+    page_views int unsigned,
+    response_time int unsigned,
+    INDEX(time),
+    INDEX (remote_ip),
+    FOREIGN KEY (instance_id) REFERENCES instance(id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+CREATE TABLE uniques_1m (
+    time datetime,
+    remote_ip varchar(255),
+    instance_id int unsigned,
+    type varchar(255),
+    value varchar(255),
+    INDEX(time),
+    INDEX(type),
+    INDEX (remote_ip),
+    FOREIGN KEY (instance_id) REFERENCES instance(id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+CREATE TABLE summary_15m (
+    time datetime,
+    remote_ip varchar(255),
+    instance_id int unsigned,
+    503_errors int unsigned,
+    500_errors int unsigned,
+    page_views int unsigned,
+    response_time int unsigned,
+    INDEX(time),
+    INDEX (remote_ip),
+    FOREIGN KEY (instance_id) REFERENCES instance(id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+CREATE TABLE uniques_15m (
+    time datetime,
+    remote_ip varchar(255),
+    instance_id int unsigned,
+    type varchar(255),
+    value varchar(255),
+    INDEX(time),
+    INDEX(type),
+    INDEX (remote_ip),
+    FOREIGN KEY (instance_id) REFERENCES instance(id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+CREATE TABLE summary_1h (
+    time datetime,
+    remote_ip varchar(255),
+    instance_id int unsigned,
+    503_errors int unsigned,
+    500_errors int unsigned,
+    page_views int unsigned,
+    response_time int unsigned,
+    INDEX(time),
+    INDEX (remote_ip),
+    FOREIGN KEY (instance_id) REFERENCES instance(id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+CREATE TABLE uniques_1h (
+    time datetime,
+    remote_ip varchar(255),
+    instance_id int unsigned,
+    type varchar(255),
+    value varchar(255),
+    INDEX(time),
+    INDEX(type),
+    INDEX (remote_ip),
+    FOREIGN KEY (instance_id) REFERENCES instance(id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+CREATE TABLE summary_1d (
+    time datetime,
+    remote_ip varchar(255),
+    instance_id int unsigned,
+    503_errors int unsigned,
+    500_errors int unsigned,
+    page_views int unsigned,
+    response_time int unsigned,
+    INDEX(time),
+    INDEX (remote_ip),
+    FOREIGN KEY (instance_id) REFERENCES instance(id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+CREATE TABLE uniques_1d (
+    time datetime,
+    remote_ip varchar(255),
+    instance_id int unsigned,
+    type varchar(255),
+    value varchar(255),
+    INDEX(time),
+    INDEX(type),
+    INDEX (remote_ip),
+    FOREIGN KEY (instance_id) REFERENCES instance(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 
--- scripts/apache_cron.php
+++ scripts/apache_cron.php
@@ -91,6 +91,7 @@ function process_apache_log($source_path, $offset, $dbcon)
   //  * Track timestamp of the last entry
   $c = 1;
   $values = array();
+  $start_ctime = null;
   $final_ctime = null;
   while(true) {
     $log_entry = stream_get_line($handle, 100000,"\n");
@@ -103,6 +104,9 @@ function process_apache_log($source_path, $offset, $dbcon)
         // Not a page load we are concerned with.
       }
       else {
+        if ($start_ctime == null) {
+          $start_ctime = strtotime($new_entry['time']);
+        }
         unset($new_entry['is_page']);
         $final_ctime = strtotime($new_entry['time']);
         $values[] = $new_entry;
@@ -125,6 +129,10 @@ function process_apache_log($source_path, $offset, $dbcon)
       }
     }
   }
+
+  // Update all the summaries affected by this time range.
+  echo "Generating summaries for requests from ".date("Y-m-d H:i:s", $start_ctime)." to ".date("Y-m-d H:i:s", $final_ctime)."\n";
+  summarize($dbcon, $start_ctime, $final_ctime);
 
   // Re-enable the foreign_key_checks and commit our work.
   $dbcon->exec("SET foreign_key_checks=1;");
@@ -245,6 +253,96 @@ function get_or_create_instance($dbcon, $servername, $install_class, $name)
 
   $INSTANCE_CACHE[$servername] = $instance;
   return $instance;
+}
+
+
+function summarize($dbcon, $start_time, $end_time)
+{
+  // Go all the way back to the last possible block that there could be new data for.
+  // Only process blocks on the even, e.g. 1:00, 1:15, 1:30
+  // Don't process blocks that end before our offical start time
+  $start_range = $start_time - $start_time % 86400;
+  while ($start_range < $end_time) {
+    $mysql_date = date("Y-m-d H:i:s", $start_range);
+
+    // Process the minute block
+    $end_range = $start_range + 60;
+    if ($end_range > $start_time) {
+      create_summary_entries($dbcon, $mysql_date, '1m', $start_range, $end_range);
+      create_uniques_entries($dbcon, $mysql_date, '1m', $start_range, $end_range);
+    }
+
+    // Process the 15 minute block every 15 minutes
+    $end_range = $start_range + 900;
+    if ($start_range % 900 == 0 && $end_range > $start_time) {
+      create_summary_entries($dbcon, $mysql_date, '15m', $start_range, $end_range);
+      create_uniques_entries($dbcon, $mysql_date, '15m', $start_range, $end_range);
+    }
+
+    // Process the hour block every hour
+    $end_range = $start_range + 3600;
+    if ($start_range % 3600 == 0 && $end_range > $start_time) {
+      create_summary_entries($dbcon, $mysql_date, '1h', $start_range, $end_range);
+      create_uniques_entries($dbcon, $mysql_date, '1h', $start_range, $end_range);
+    }
+
+    // Process the day block every day
+    $end_range = $start_range + 86400;
+    if ($start_range % 86400 == 0 && $end_range > $start_time) {
+      create_summary_entries($dbcon, $mysql_date, '1d', $start_range, $end_range);
+      create_uniques_entries($dbcon, $mysql_date, '1d', $start_range, $end_range);
+    }
+
+    // Step up minute by minute
+    $start_range += 60;
+  }
+}
+
+
+function create_summary_entries($dbcon, $mysql_date, $table_suffix, $start_range, $end_range)
+{
+  $result = $dbcon->query("
+      SELECT
+        instance_id,
+        remote_ip,
+        count(*) as page_views,
+        IFNULL(sum(response_code = 503),0) as 503_errors,
+        IFNULL(sum(response_code = 500),0) as 500_errors,
+        IFNULL(sum(response_time), 0) as response_time
+      FROM request
+      WHERE time BETWEEN FROM_UNIXTIME($start_range) AND FROM_UNIXTIME($end_range)
+      GROUP BY instance_id, remote_ip
+  ");
+  $rows = array();
+  while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+    $row['time'] = $mysql_date;
+    $rows[] = $row;
+  }
+  $result->closeCursor();
+  insert_batch($dbcon, "summary_$table_suffix", $rows);
+}
+
+
+function create_uniques_entries($dbcon, $mysql_date, $table_suffix, $start_range, $end_range)
+{
+  $mysql_date = date("Y-m-d H:i:s", $start_range);
+  $rows = array();
+  foreach(array('path') as $stat) {
+    $result = $dbcon->query("
+      SELECT instance_id, remote_ip, $stat as value
+      FROM request
+      WHERE time BETWEEN FROM_UNIXTIME($start_range) AND FROM_UNIXTIME($end_range)
+      GROUP BY instance_id, remote_ip, $stat
+    ");
+
+    while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+      $row['time'] = $mysql_date;
+      $row['type'] = $stat;
+      $rows[] = $row;
+    }
+    $result->closeCursor();
+  }
+  insert_batch($dbcon, "uniques_$table_suffix", $rows);
 }
 
 ?>
