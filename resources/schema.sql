0cc9bac0b295f697ed05a30463956101593cca87
Graylin Kim <graylin.kim@gmail.com>
2014-03-07 15:14:21 -0500

    WIP

--- resources/schema.sql
+++ resources/schema.sql
@@ -1,4 +1,3 @@
-DROP TABLE IF EXISTS mysql;
 DROP TABLE IF EXISTS datatable;
 DROP TABLE IF EXISTS request;
 DROP TABLE IF EXISTS summary_1m;
@@ -12,7 +11,11 @@ DROP TABLE IF EXISTS uniques_1d;
 DROP TABLE IF EXISTS instance;
 DROP TABLE IF EXISTS apache_cron_runs;
 DROP TABLE IF EXISTS mysql_cron_runs;
-
+DROP TABLE IF EXISTS mysql;
+DROP TABLE IF EXISTS mysql_1m;
+DROP TABLE IF EXISTS mysql_15m;
+DROP TABLE IF EXISTS mysql_1h;
+DROP TABLE IF EXISTS mysql_1d;
 CREATE TABLE mysql (
   time datetime PRIMARY KEY,
   aborted_connects int unsigned,
@@ -50,9 +53,168 @@ CREATE TABLE mysql (
   threads_connected int unsigned,
   threads_created int unsigned,
   threads_running int unsigned,
-  uptime int unsigned,
+  uptime int unsigned
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+CREATE TABLE mysql_1m (
+  time datetime PRIMARY KEY,
+  aborted_connects int unsigned,
+  bytes_received int unsigned,
+  bytes_sent int unsigned,
+  connections int unsigned,
+  created_tmp_disk_tables int unsigned,
+  created_tmp_files int unsigned,
+  created_tmp_tables int unsigned,
+  delayed_errors int unsigned,
+  delayed_insert_threads int unsigned,
+  delayed_writes int unsigned,
+  innodb_row_lock_current_waits int unsigned,
+  innodb_row_lock_time int unsigned,
+  innodb_row_lock_time_avg int unsigned,
+  innodb_row_lock_time_max int unsigned,
+  innodb_row_lock_waits int unsigned,
+  innodb_rows_deleted int unsigned,
+  innodb_rows_inserted int unsigned,
+  innodb_rows_read int unsigned,
+  innodb_rows_updated int unsigned,
+  max_used_connections int unsigned,
+  open_files int unsigned,
+  open_streams int unsigned,
+  open_table_definitions int unsigned,
+  open_tables int unsigned,
+  opened_files int unsigned,
+  queries int unsigned,
+  questions int unsigned,
+  slow_launch_threads int unsigned,
+  slow_queries int unsigned,
+  table_locks_immediate int unsigned,
+  table_locks_waited int unsigned,
+  threads_cached int unsigned,
+  threads_connected int unsigned,
+  threads_created int unsigned,
+  threads_running int unsigned,
+  availability float unsigned
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE mysql_15m (
+  time datetime PRIMARY KEY,
+  aborted_connects int unsigned,
+  bytes_received int unsigned,
+  bytes_sent int unsigned,
+  connections int unsigned,
+  created_tmp_disk_tables int unsigned,
+  created_tmp_files int unsigned,
+  created_tmp_tables int unsigned,
+  delayed_errors int unsigned,
+  delayed_insert_threads int unsigned,
+  delayed_writes int unsigned,
+  innodb_row_lock_current_waits int unsigned,
+  innodb_row_lock_time int unsigned,
+  innodb_row_lock_time_avg int unsigned,
+  innodb_row_lock_time_max int unsigned,
+  innodb_row_lock_waits int unsigned,
+  innodb_rows_deleted int unsigned,
+  innodb_rows_inserted int unsigned,
+  innodb_rows_read int unsigned,
+  innodb_rows_updated int unsigned,
+  max_used_connections int unsigned,
+  open_files int unsigned,
+  open_streams int unsigned,
+  open_table_definitions int unsigned,
+  open_tables int unsigned,
+  opened_files int unsigned,
+  queries int unsigned,
+  questions int unsigned,
+  slow_launch_threads int unsigned,
+  slow_queries int unsigned,
+  table_locks_immediate int unsigned,
+  table_locks_waited int unsigned,
+  threads_cached int unsigned,
+  threads_connected int unsigned,
+  threads_created int unsigned,
+  threads_running int unsigned,
+  availability float unsigned
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE mysql_1h (
+  time datetime PRIMARY KEY,
+  aborted_connects int unsigned,
+  bytes_received int unsigned,
+  bytes_sent int unsigned,
+  connections int unsigned,
+  created_tmp_disk_tables int unsigned,
+  created_tmp_files int unsigned,
+  created_tmp_tables int unsigned,
+  delayed_errors int unsigned,
+  delayed_insert_threads int unsigned,
+  delayed_writes int unsigned,
+  innodb_row_lock_current_waits int unsigned,
+  innodb_row_lock_time int unsigned,
+  innodb_row_lock_time_avg int unsigned,
+  innodb_row_lock_time_max int unsigned,
+  innodb_row_lock_waits int unsigned,
+  innodb_rows_deleted int unsigned,
+  innodb_rows_inserted int unsigned,
+  innodb_rows_read int unsigned,
+  innodb_rows_updated int unsigned,
+  max_used_connections int unsigned,
+  open_files int unsigned,
+  open_streams int unsigned,
+  open_table_definitions int unsigned,
+  open_tables int unsigned,
+  opened_files int unsigned,
+  queries int unsigned,
+  questions int unsigned,
+  slow_launch_threads int unsigned,
+  slow_queries int unsigned,
+  table_locks_immediate int unsigned,
+  table_locks_waited int unsigned,
+  threads_cached int unsigned,
+  threads_connected int unsigned,
+  threads_created int unsigned,
+  threads_running int unsigned,
+  availability float unsigned
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE mysql_1d (
+  time datetime PRIMARY KEY,
+  aborted_connects int unsigned,
+  bytes_received int unsigned,
+  bytes_sent int unsigned,
+  connections int unsigned,
+  created_tmp_disk_tables int unsigned,
+  created_tmp_files int unsigned,
+  created_tmp_tables int unsigned,
+  delayed_errors int unsigned,
+  delayed_insert_threads int unsigned,
+  delayed_writes int unsigned,
+  innodb_row_lock_current_waits int unsigned,
+  innodb_row_lock_time int unsigned,
+  innodb_row_lock_time_avg int unsigned,
+  innodb_row_lock_time_max int unsigned,
+  innodb_row_lock_waits int unsigned,
+  innodb_rows_deleted int unsigned,
+  innodb_rows_inserted int unsigned,
+  innodb_rows_read int unsigned,
+  innodb_rows_updated int unsigned,
+  max_used_connections int unsigned,
+  open_files int unsigned,
+  open_streams int unsigned,
+  open_table_definitions int unsigned,
+  open_tables int unsigned,
+  opened_files int unsigned,
+  queries int unsigned,
+  questions int unsigned,
+  slow_launch_threads int unsigned,
+  slow_queries int unsigned,
+  table_locks_immediate int unsigned,
+  table_locks_waited int unsigned,
+  threads_cached int unsigned,
+  threads_connected int unsigned,
+  threads_created int unsigned,
+  threads_running int unsigned,
+  availability float unsigned
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 CREATE TABLE datatable (
     id int unsigned AUTO_INCREMENT PRIMARY KEY,
@@ -205,3 +367,4 @@ CREATE TABLE apache_cron_runs (
 
 -- Insert default values to make life easier...
 INSERT INTO apache_cron_runs VALUES (0, '2013-01-01 00:00:00');
+
--- scripts/mysql_cron.php
+++ scripts/mysql_cron.php
@@ -2,6 +2,7 @@
 date_default_timezone_set('America/New_York');
 require(realpath(dirname(__FILE__).'/../lib/utils.php'));
 
+$IGNORED_STATS = "/^SSL_|^SLAVE_|^PERFORMANCE_|^HANDLER_|^COM_|^BINLOG_|^INNODB_B|^INNODB_D|^INNODB_H|^INNODB_L|^INNODB_O|^INNODB_P|^INNODB_T|^COMPRESSION|^TC_|^OPENED_TABLE|^PREPARED_STMT|^RPL_STATUS|^ABORTED_CL|^KEY_|^LAST_QUERY_COST|^FLUSH_|^NOT_FLUSHED|^SORT_|^SELECT_|^UPTIME_|^QCACHE_/i";
 ///////////////////////////////
 // Bootstrap the environment
 ///////////////////////////////
@@ -22,33 +23,134 @@ if ($dbcon === false)
   exit(1);
 }
 
+
+$now = time();
+$time = $now - ($now % 60);
+
 $result = $dbcon->query("SELECT * FROM mysql ORDER BY time DESC LIMIT 1");
-$row = $result->fetch(PDO::FETCH_ASSOC);
-$start_ctime = strtotime($row['final_ctime']);
-$mysql_date = date("Y-m-d H:i:s", $final_ctime);
+$old_entry = $result->fetch(PDO::FETCH_ASSOC);
 
-echo "Last MYSQL run ended at ".DateTime::createFromFormat('U', $start_ctime)->format(DateTime::ISO8601)."\n";
-
-if ((intval($final_ctime) - $start_ctime) > 65) {
-  // The oldest record is over 65 seconds old, we had some downtime
-  echo "Last run was ".(intval($final_ctime) - $start_ctime)." seconds ago\n";
-}
-
-// get current stats
+$new_entry = array('time'=>date("Y-m-d H:i:s", $time));
 $result = $dbcon->query("SELECT * from information_schema.SESSION_STATUS");
-while($sub = $result->fetch(PDO::FETCH_OBJ))
-{
-  $out[strtolower($sub->VARIABLE_NAME)] = $sub->VARIABLE_VALUE;
-}
-
-// Clean out the stuff we don't want
-foreach($out as $key => $value)
-{
-  if(preg_match("/^SSL_|^SLAVE_|^PERFORMANCE_|^HANDLER_|^COM_|^BINLOG_|^INNODB_B|^INNODB_D|^INNODB_H|^INNODB_L|^INNODB_O|^INNODB_P|^INNODB_T|^COMPRESSION|^TC_|^OPENED_TABLE|^PREPARED_STMT|^RPL_STATUS|^ABORTED_CL|^KEY_|^LAST_QUERY_COST|^FLUSH_|^NOT_FLUSHED|^SORT_|^SELECT_|^UPTIME_|^QCACHE_/i",$key))
-  {
-    unset($out[$key]);
+while($stat = $result->fetch(PDO::FETCH_ASSOC)) {
+  if (!preg_match($IGNORED_STATS, $stat['VARIABLE_NAME'])) {
+    $new_entry[strtolower($stat['VARIABLE_NAME'])] = $stat['VARIABLE_VALUE'];
   }
 }
 
-$dbcon->exec("INSERT INTO mysql (aborted_connects, bytes_received, bytes_sent, connections, created_tmp_disk_tables, created_tmp_files, created_tmp_tables, delayed_errors, delayed_insert_threads, delayed_writes, innodb_row_lock_current_waits, innodb_row_lock_time, innodb_row_lock_time_avg, innodb_row_lock_time_max, innodb_row_lock_waits, innodb_rows_deleted, innodb_rows_inserted, innodb_rows_read, innodb_rows_updated, max_used_connections, open_files, open_streams, open_table_definitions, open_tables, opened_files, queries, questions, slow_launch_threads, slow_queries, table_locks_immediate, table_locks_waited, threads_cached, threads_connected, threads_created, threads_running, uptime, time) VALUES ({$out['aborted_connects']}, {$out['bytes_received']}, {$out['bytes_sent']}, {$out['connections']}, {$out['created_tmp_disk_tables']}, {$out['created_tmp_files']}, {$out['created_tmp_tables']}, {$out['delayed_errors']}, {$out['delayed_insert_threads']}, {$out['delayed_writes']}, {$out['innodb_row_lock_current_waits']}, {$out['innodb_row_lock_time']}, {$out['innodb_row_lock_time_avg']}, {$out['innodb_row_lock_time_max']}, {$out['innodb_row_lock_waits']}, {$out['innodb_rows_deleted']}, {$out['innodb_rows_inserted']}, {$out['innodb_rows_read']}, {$out['innodb_rows_updated']}, {$out['max_used_connections']}, {$out['open_files']}, {$out['open_streams']}, {$out['open_table_definitions']}, {$out['open_tables']}, {$out['opened_files']}, {$out['queries']}, {$out['questions']}, {$out['slow_launch_threads']}, {$out['slow_queries']}, {$out['table_locks_immediate']}, {$out['table_locks_waited']}, {$out['threads_cached']}, {$out['threads_connected']}, {$out['threads_created']}, {$out['threads_running']}, {$out['uptime']}, '{$mysql_date}' );");
+insert_batch($dbcon, 'mysql', array($new_entry));
 
+if (!$old_entry) {
+  echo "first entry! Exiting.. \n";
+  exit(0);
+}
+
+$rows = array();
+$summary_keys = array_keys($old_entry);
+$summary_keys[array_search('uptime', $summary_keys)] = 'availability';
+$time_diff = $time - strtotime($old_entry['time']);
+$start = strtotime($old_entry['time']);
+while (($start+60) < $time) {
+  // Insert blank entries for the missed time.
+  $row = array_combine($summary_keys, array_fill(0, count($summary_keys), '?'));
+  $row['time'] = date("Y-m-d H:i:s", $start);
+  $rows[] = $row;
+}
+
+// Construct our new non-blank summary entry
+if ($new_entry['uptime'] < $time_diff) {
+  // There was server downtime, the new values must be for this minute
+  $row = $new_entry;
+}
+else {
+  $row = array();
+  foreach (array_keys($new_entry) as $key) {
+    $row[$key] = $new_entry[$key] - $old_entry[$key];
+  }
+}
+
+$row['time'] = date("Y-m-d H:i:s", $start);
+$row['availability'] = $row['uptime'] > 60 ? 1.00 : $row['uptime']/60.0;
+unset($row['uptime']);
+$rows[] = $row;
+insert_batch($dbcon, 'mysql_1m', $rows);
+
+
+function summarize($dbcon, $start_time, $end_time)
+{
+  // Go all the way back to the last possible block that there could be new data for.
+  // Only process blocks on the even, e.g. 1:00, 1:15, 1:30
+  // Don't process blocks that end before our offical start time
+  $start_range = strtotime('today', $start_time);
+  while ($start_range < $end_time) {
+    $mysql_date = date("Y-m-d H:i:s", $start_range);
+
+    // Process the 15 minute block every 15 minutes
+    $end_range = $start_range + 900;
+    if ($start_range % 900 == 0 && $end_range > $start_time) {
+      create_summary_entries($dbcon, $mysql_date, '15m', $start_range, $end_range);
+    }
+
+    // Process the hour block every hour
+    $end_range = $start_range + 3600;
+    if ($start_range % 3600 == 0 && $end_range > $start_time) {
+      create_summary_entries($dbcon, $mysql_date, '1h', $start_range, $end_range);
+    }
+
+    // Process the day block every day
+    $end_range = strtotime('today', $start_range + 86400);
+    if (strtotime('today', $start_range) == $start_range && $end_range > $start_time) {
+      create_summary_entries($dbcon, $mysql_date, '1d', $start_range, $end_range);
+    }
+
+    // Step up minute by minute
+    $start_range += 60;
+  }
+}
+
+
+function create_summary_entries($dbcon, $mysql_date, $table_suffix, $start_range, $end_range) {
+  $dbcon->query("
+    INSERT INTO mysql_$table_suffix VALUES
+      SELECT
+        '$mysql_date',
+        sum(aborted_connects),
+        sum(bytes_received),
+        sum(bytes_sent),
+        sum(connections),
+        sum(created_tmp_disk_tables),
+        sum(created_tmp_files),
+        sum(created_tmp_tables),
+        sum(delayed_errors),
+        sum(delayed_insert_threads),
+        sum(delayed_writes),
+        sum(innodb_row_lock_current_waits),
+        sum(innodb_row_lock_time),
+        sum(innodb_row_lock_time_avg),
+        sum(innodb_row_lock_time_max),
+        sum(innodb_row_lock_waits),
+        sum(innodb_rows_deleted),
+        sum(innodb_rows_inserted),
+        sum(innodb_rows_read),
+        sum(innodb_rows_updated),
+        sum(max_used_connections),
+        sum(open_files),
+        sum(open_streams),
+        sum(open_table_definitions),
+        sum(open_tables),
+        sum(opened_files),
+        sum(queries),
+        sum(questions),
+        sum(slow_launch_threads),
+        sum(slow_queries),
+        sum(table_locks_immediate),
+        sum(table_locks_waited),
+        sum(threads_cached),
+        sum(threads_connected),
+        sum(threads_created),
+        sum(threads_running),
+        sum(availability)/count(*),
+      FROM mysql_1m
+      WHERE time BETWEEN FROM_UNIXTIME($start_range) AND FROM_UNIXTIME($end_range)"
+  );
+}
