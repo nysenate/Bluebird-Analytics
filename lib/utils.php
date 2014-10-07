37793fd3c45b79ac597f3606672f84eacb2784f2
routinet <binkowsk@nysenate.gov>
2014-10-07 10:43:01 -0400

    Add 'last updated' data point to footer

--- lib/utils.php
+++ lib/utils.php
@@ -194,4 +194,15 @@ function load_bluebird_instances($config, $force = false)
   return $instances;
 } // load_bluebird_instances()
 
+function fetch_last_update_time($dbcon) {
+  $query = "SELECT MAX(final_ctime) AS last_update FROM apache_cron_runs";
+  try {
+    $last_update = $dbcon->query($query)->fetch()['last_update'];
+  }
+  catch (Exception $e) {
+    $last_update = 'Unknown<br />(ERR: '.$e->getCode().')';
+  }
+  if (!$last_update) { $last_update = "Unknown<br />(No Data Returned)"; }
+  return $last_update;
+}
 ?>
